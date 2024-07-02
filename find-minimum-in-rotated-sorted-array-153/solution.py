0bf265b50afb16c92374d9cec3f8ca812dd515c4
Markus Sekhon <markus.sekhon@pm.me>
2024-07-02 16:36:16 -0700

    find min in rotated sorted array

---
+++ find-minimum-in-rotated-sorted-array-153/solution.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if len(nums) = 1:
+            return nums[0]
+
+        left = 0
+        right = len(nums) - 1
+        midpoint = lambda low, high : low + (high-low)//2
+
+        while right-left != 1:
+            middle = midpoint(left, right)
+
+            if nums[middle] < nums[right]:
+                right = middle
+            else:
+                left = middle
+
+        return min(nums[left],nums[right])
+
+sol = Solution()
+nums = [7,1,2,3,4,5,6]
+print(sol.findMin(nums))
